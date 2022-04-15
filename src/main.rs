54abc526d7c0c01037977c75a04463867e13a662
Boban Acimovic <boban.acimovic@gmail.com>
2022-04-15 03:12:35 +0200

    impl error handling

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 //! Crate vanity implements Go vanity imports HTTP server.
-#![warn(missing_debug_implementations, rust_2018_idioms, missing_docs)]
+#![warn(missing_debug_implementations, rust_2018_idioms)]
+// #![warn(missing_debug_implementations, rust_2018_idioms, missing_docs)]
 
 use anyhow::Result;
 use axum::{
     extract::{Extension, Path},
-    response::{Html, IntoResponse},
+    response::{Html, IntoResponse, Response},
     routing::get,
     Json, Router,
 };
@@ -72,14 +73,19 @@ type SharedState = Arc<RwLock<Cfg>>;
 async fn vanity(
     Path(package): Path<String>,
     Extension(state): Extension<SharedState>,
-) -> impl IntoResponse {
-    let s = &state.read().unwrap();
-    Html(format!(
-        r#"<!DOCTYPE html><html><head><meta name="go-import" content="{}/{} git {}"></head><body>Nothing to see here.</body></html>"#,
-        s.domain,
-        package,
-        s.packages.get(&package).unwrap(),
-    ))
+) -> Result<Html<String>, VanityError> {
+    let s = &state.read().map_err(|err| {
+        eprintln!("error: {}", err); // TODO: replace this with slog
+        VanityError::Poisoned
+    })?;
+
+    match s.packages.get(&package) {
+        Some(repository) => Ok(Html(format!(
+            r#"<!DOCTYPE html><html><head><meta name="go-import" content="{}/{} git {}"></head><body>Nothing to see here.</body></html>"#,
+            s.domain, package, repository,
+        ))),
+        None => Err(VanityError::NotFound),
+    }
 }
 
 #[derive(Debug, Deserialize, Clone)]
@@ -91,4 +97,16 @@ struct Cfg {
 #[derive(Serialize)]
 struct Status<'a> {
     status: &'a str,
+}
+
+#[derive(Debug)]
+pub enum VanityError {
+    Poisoned,
+    NotFound,
+}
+
+impl IntoResponse for VanityError {
+    fn into_response(self) -> Response {
+        unimplemented!()
+    }
 }
